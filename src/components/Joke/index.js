4d0132fa3cb705b1e63fad20e1a16ea49be498c3
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-05-29 11:49:36 +0200

    DA Test: úkol 15

--- src/components/Joke/index.js
+++ src/components/Joke/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+
 import './style.css';
 
-export const Joke = (props) => {
-  const [like, setLike] = useState(0);
-  const [dislike, setDislike] = useState(0);
+export const Joke = ({ userAvatar, userName, text, likes, dislikes }) => {
+  const [like, setLike] = useState(likes);
+  const [dislike, setDislike] = useState(dislikes);
 
   const handleLike = () => {
     setLike(like + 1);
@@ -17,18 +18,11 @@ export const Joke = (props) => {
     <div className="joke">
       <div className="joke__body">
         <div className="joke__user">
-          <img
-            className="user-avatar"
-            src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-          />
-          <p className="user-name">Neroxx</p>
+          <img className="user-avatar" src={userAvatar} />
+          <p className="user-name">{userName}</p>
         </div>
 
-        <p className="joke__text">
-          The secret service isn't allowed to yell "Get down!" anymore when the
-          president is about to be attacked. Now they have to yell "Donald,
-          duck!"
-        </p>
+        <p className="joke__text">{text}</p>
       </div>
       <div className="joke__likes">
         <button
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Joke } from './components/Joke';
+import { jokes } from './jokes';
+
 import './style.css';
 
 const App = () => (
   <div className="container">
-    <Joke />
+    {jokes.map((joke) => (
+      <Joke
+        userAvatar={joke.avatar}
+        userName={joke.name}
+        text={joke.text}
+        key={joke.id}
+        likes={joke.likes}
+        dislikes={joke.dislikes}
+      />
+    ))}
   </div>
 );
 
